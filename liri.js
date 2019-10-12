d72a69e32f61b04a437816e6049befbd68f5c7d9
NickForma <nickforma60@gmail.com>
2019-10-12 18:25:18 -0400

    inquirer

--- liri.js
+++ liri.js
@@ -1,9 +1,24 @@
 let dotenv = require("dotenv").config();
+let inquirer = require("inquirer");
+//var keys = require("./keys.js");
 
-var keys = require("./keys.js");
+// var spotify = new Spotify(keys.spotify);
 
-var spotify = new Spotify(keys.spotify);
+// let key = process.env;
 
-
-
-let key = process.env.
+inquirer.prompt([
+  {
+    type: "input",
+    name: "name",
+    message: "What's your name?"
+  },
+  {
+    type: "list",
+    name: "search",
+    message: "What do you want to search for?",
+    choices: ["concert", "song", "movie"]
+  }
+]).then((user)=> {
+    console.log(user.name)
+    console.log(user.search)
+});
